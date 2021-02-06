bee5e12fd7f75da4452e1b08f2d2e5de60528c99
Marina Grishina <52827985+AniramOcean@users.noreply.github.com>
2021-02-06 20:15:58 +0300

    feat: implement vigenere cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,41 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type = true) {
+    this.type = type;
+  }
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) throw new Error('')
+
+    message = [...message.toUpperCase()]
+    key = [...key.toUpperCase()]
+    let i = 0
+
+    let array = message.map(el => {
+      const elChar = el.charCodeAt()
+      return elChar >= 65 && elChar <= 90
+          ? String.fromCharCode(((elChar - 65 + key[i++ % key.length].charCodeAt() - 65) % 26) + 65)
+          : el
+    })
+
+    return this.type ? array.join('') : array.reverse().join('')
+  }
+
+  decrypt(message, key) {
+    if (message === undefined || key === undefined) throw new Error('')
+
+    message = [...message.toUpperCase()]
+    key = [...key.toUpperCase()]
+    let i = 0
+
+    let array = message.map(el => {
+      const elChar = el.charCodeAt();
+      return elChar >= 65 && elChar <= 90
+          ? String.fromCharCode((elChar - 65 - (key[i++ % key.length].charCodeAt() - 65) + 26) % 26 + 65)
+          : el;
+    })
+
+    return this.type ? array.join('') : array.reverse().join('')
   }
 }
 
