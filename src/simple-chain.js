b3d5891d24ca08d5afbe6df10acfd8383a0a2ae1
Marina Grishina <52827985+AniramOcean@users.noreply.github.com>
2021-02-06 16:55:42 +0300

    feat: implement simple-chain solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -30,38 +30,4 @@ const chainMaker = {
   }
 };
 
-// const chainMaker = {
-//   result: [],
-//   getLength() {
-//     return this.result.length
-//   },
-//   addLink(value) {
-//     value = String(value) ? `( ${String(value)} )` : '( )'
-//     this.result.push(value)
-//
-//     return this
-//   },
-//   removeLink(position) {
-//     if (!position || typeof position !== 'number' || position < 0 ) {
-//       this.result = []
-//       throw new Error()
-//     }
-//
-//     this.result.splice(position - 1, 1)
-//     return this
-//   },
-//   reverseChain() {
-//     this.result.reverse()
-//     return this
-//   },
-//   finishChain() {
-//     const finishResult = this.result.join('~~')
-//     this.result = []
-//
-//     return finishResult
-//   }
-// };
-
 module.exports = chainMaker;
-
-// console.log(chainMaker.addLink(0).addLink(NaN).addLink(123).reverseChain().addLink('tergrge').finishChain());
